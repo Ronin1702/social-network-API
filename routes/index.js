e386227ce5fea756e95d35be52a8187e288d1d0e
Ronin1702 <kchen@me.com>
2023-09-01 07:06:24 -0400

    send 404 signal to wrong route

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
+// Import required modules
 const router = require('express').Router();
 const apiRoutes = require('./api');
 
+// API Routes
 router.use('/api', apiRoutes);
 
+// Handle wrong routes
 router.use((req, res) => {
-  return res.send('Wrong route!');
+  res.status(404).send('Wrong route!');
 });
 
+// Export the router
 module.exports = router;
